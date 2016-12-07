97ad20a3e33fb19ee2e86283d09b971b47b3b974
Justin Clareburt <justcla@microsoft.com>
2016-12-07 13:15:27 -0500

    Added settings margin with context menu.

---
+++ HotSettings/Margins/SettingsMarginFactory.cs
@@ -0,0 +1,45 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="SettingsMarginFactory.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.VisualStudio.Shell;
+
+namespace HotSettings
+{
+    /// <summary>
+    /// Export a <see cref="IWpfTextViewMarginProvider"/>, which returns an instance of the margin for the editor to use.
+    /// </summary>
+    [Export(typeof(IWpfTextViewMarginProvider))]
+    [Name(SettingsMargin.MarginName)]
+    [Order(Before = PredefinedMarginNames.Glyph)]
+    [MarginContainer(PredefinedMarginNames.Left)]
+    [ContentType("text")]                                       // Show this margin for all text-based types
+    [TextViewRole(PredefinedTextViewRoles.Interactive)]
+    internal sealed class SettingsMarginFactory : IWpfTextViewMarginProvider
+    {
+        #region IWpfTextViewMarginProvider
+
+        [Import]
+        internal SVsServiceProvider ServiceProvider;
+
+        /// <summary>
+        /// Creates an <see cref="IWpfTextViewMargin"/> for the given <see cref="IWpfTextViewHost"/>.
+        /// </summary>
+        /// <param name="wpfTextViewHost">The <see cref="IWpfTextViewHost"/> for which to create the <see cref="IWpfTextViewMargin"/>.</param>
+        /// <param name="marginContainer">The margin that will contain the newly-created margin.</param>
+        /// <returns>The <see cref="IWpfTextViewMargin"/>.
+        /// The value may be null if this <see cref="IWpfTextViewMarginProvider"/> does not participate for this context.
+        /// </returns>
+        public IWpfTextViewMargin CreateMargin(IWpfTextViewHost wpfTextViewHost, IWpfTextViewMargin marginContainer)
+        {
+            return new SettingsMargin(wpfTextViewHost.TextView, this.ServiceProvider);
+        }
+
+        #endregion
+    }
+}
