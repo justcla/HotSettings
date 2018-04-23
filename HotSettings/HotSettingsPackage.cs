60a53d13d77f8ec5473c67f13e892c0db7d5c925
Mads Kristensen <post@madskristensen.net>
2018-04-23 14:04:52 -0700

    Converted to AsyncPackage

--- HotSettings/HotSettingsPackage.cs
+++ HotSettings/HotSettingsPackage.cs
@@ -3,6 +3,8 @@ using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
 
 namespace HotSettings
 {
@@ -23,46 +25,27 @@ namespace HotSettings
     /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
     /// </para>
     /// </remarks>
-    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
     [ProvideMenuResource("Menus.ctmenu", 1)]
     //[ProvideAutoLoad(VSConstants.UICONTEXT.NoSolution_string)]
     //[ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExists_string)]
-    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionHasMultipleProjects_string)]
-    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionHasSingleProject_string)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionHasMultipleProjects_string, PackageAutoLoadFlags.BackgroundLoad)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionHasSingleProject_string, PackageAutoLoadFlags.BackgroundLoad)]
     [Guid(HotSettingsPackage.PackageGuidString)]
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
-    public sealed class HotSettingsPackage : Package
+    public sealed class HotSettingsPackage : AsyncPackage
     {
         /// <summary>
         /// HotSettings GUID string.
         /// </summary>
         public const string PackageGuidString = "d570a6f3-b0ad-42c3-b71f-db7c6b3d39c0";
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="global::HotSettings"/> class.
-        /// </summary>
-        public HotSettingsPackage()
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
-            // Inside this method you can place any initialization code that does not require
-            // any Visual Studio service because at this point the package object is created but
-            // not sited yet inside Visual Studio environment. The place to do all the other
-            // initialization is the Initialize method.
-        }
-
-        #region Package Members
-
-        /// <summary>
-        /// Initialization of the package; this method is called right after the package is sited, so this is the place
-        /// where you can put all the initialization code that rely on services provided by VisualStudio.
-        /// </summary>
-        protected override void Initialize()
-        {
-            base.Initialize();
+            await JoinableTaskFactory.SwitchToMainThreadAsync();
             HotSettingsCommandHandler.Initialize(this);
             TrackActiveItemsCommandHandler.Initialize(this);
         }
-
-        #endregion
     }
 }
